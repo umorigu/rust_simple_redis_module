cc178943f276205ecd1bce6b1b24104cafccfe2d
umorigu <umorigu@gmail.com>
2019-03-20 14:48:06 +0000

    Minimum Redis module inplementation

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,12 @@ const REDISMODULE_APIVER_1: c_int = 1;
 pub enum RedisModuleCtx {}
 pub enum RedisModuleString {}
 
+pub type RedisModuleCmdFunc = extern "C" fn(
+    ctx: *mut RedisModuleCtx,
+    argv: *mut *mut RedisModuleString,
+    argc: c_int,
+) -> c_int;
+
 extern "C" {
     #[allow(improper_ctypes)]
     #[link(name = "redismodule", kind = "static")]
@@ -20,6 +26,36 @@ extern "C" {
         module_version: c_int,
         api_version: c_int,
     ) -> c_int;
+
+    // int RedisModule_CreateCommand(RedisModuleCtx *ctx, const char *name,
+    //   RedisModuleCmdFunc cmdfunc, const char *strflags, int firstkey,
+    //   int lastkey, int keystep);
+    static RedisModule_CreateCommand: extern "C" fn(
+        ctx: *mut RedisModuleCtx,
+        name: *const u8,
+        fmdfunc: RedisModuleCmdFunc,
+        strflags: *const u8,
+        firstkey: c_int,
+        lastkey: c_int,
+        keystep: c_int,
+    ) -> c_int;
+
+    // int RedisModule_ReplyWithStringBuffer(RedisModuleCtx *ctx,
+    //   const char *buf, size_t len);
+    static RedisModule_ReplyWithStringBuffer:
+        extern "C" fn(ctx: *mut RedisModuleCtx, str: *const u8, len: size_t) -> c_int;
+}
+
+extern "C" fn RustHello_RedisCommand(
+    ctx: *mut RedisModuleCtx,
+    argv: *mut *mut RedisModuleString,
+    argc: c_int,
+) -> c_int {
+    unsafe {
+        const HELLO: &'static str = "hello";
+        RedisModule_ReplyWithStringBuffer(ctx, format!("{}", HELLO).as_ptr(), HELLO.len());
+    }
+    return REDISMODULE_OK;
 }
 
 #[no_mangle]
@@ -35,6 +71,18 @@ pub extern "C" fn RedisModule_OnLoad(
             1,
             REDISMODULE_APIVER_1,
         );
+        if RedisModule_CreateCommand(
+            ctx,
+            format!("{}\0", "rusthello").as_ptr(),
+            RustHello_RedisCommand,
+            format!("{}\0", "readonly").as_ptr(),
+            0,
+            0,
+            0,
+        ) == REDISMODULE_ERR
+        {
+            return REDISMODULE_ERR;
+        }
     }
     return REDISMODULE_OK;
 }
