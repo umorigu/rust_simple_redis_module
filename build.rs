939b0215e934489bd6c035fdb429a9fc7d4d1f2f
umorigu <umorigu@gmail.com>
2019-03-20 14:32:51 +0000

    Add redismodule.c to call RedisModule_Init() in redismodule.h
    * redismodule.c to call RedisModule_Init() in redismodule.h
    * Set create-type: dylib (shared library: xxx.so)
    * Use cc crate to build C files

---
+++ build.rs
@@ -0,0 +1,8 @@
+extern crate cc;
+
+fn main() {
+    cc::Build::new()
+        .file("src/redismodule.c")
+        .include("include/")
+        .compile("libredismodule.a");
+}
---
+++ src/redismodule.c
@@ -0,0 +1,6 @@
+#include "include/redismodule.h"
+
+int Export_RedisModule_Init(RedisModuleCtx *ctx,
+    const char *name, int ver, int apiver) {
+    return RedisModule_Init(ctx, name, ver, apiver);
+}
